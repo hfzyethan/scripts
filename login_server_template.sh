2bc836eb7b9769db7c81254d3da35774e2e32b12
Yang Zhou <50588071+hfzyethan@users.noreply.github.com>
2019-05-26 23:35:46 +0800

    Create login_server_template.sh

---
+++ login_server_template.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/expect
+
+##################################################
+#              安装 expect
+# Ubuntu: sudo apt-get install -y tcl tk expect
+# CentOS: sudo yum install -y expect
+# MacOS: brew install expect
+##################################################
+
+# 设置超时时间 3 秒
+set timeout 3
+
+# fork一个子进程执行 ssh
+spawn ssh user_name@ip
+
+# 期待匹配到 'user_name@ip_string's password:'
+expect "*password*"
+
+# 向命令行输入密码，并回车
+send "<password>\r"
+
+# 允许用户与命令行交互
+interact
